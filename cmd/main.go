808ba23b0a00c5ead77a9df605e9186c7d1d26c7
ljun20160606 <ljun20160606@gmail.com>
2019-01-29 15:06:02 +0800

    refactor: cli支持只更新readme

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+var app = cli.NewApp()
+
+func init() {
+	di.Put(app)
+}
+
 // 1. 生成leetcode.json
 // 2. 根据题目生成题目所需文件
 //   (1) 判断文件是否在字典中
@@ -18,11 +24,9 @@ func main() {
 		log.Fatal(err)
 	}
 	di.Start()
-	app := cli.NewApp()
 	app.Name = "leetcode"
 	app.Version = "0.3.0"
 	app.Usage = "leetcode刷题辅助工具"
-	app.Commands = append(lc.Commands)
 	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
